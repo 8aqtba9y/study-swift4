c11f1b02c1bbb2e211677156d39f777cf9adbd9d
8aqtba9y <8aqtba9y@gmail.com>
2018-01-17 00:48:27 +0900

    [add] 09_TabbarEx

---
+++ 09_TabbarEX/TabbarEX/TabbarEX/FirstViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FirstViewController.swift
+//  TabbarEX
+//
+//  Created by Qi Jsb on 2018/01/17.
+//  Copyright © 2018年 Qi Jsb. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonAction(_ sender: Any) {
+        outputLabel.text = "1 button is on clicked!"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
